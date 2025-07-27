2c91489d1bde2e7b105b5c361fcfc193186fb9e9
shaeespring <shaeespring@gmail.com>
2025-07-26 20:07:11 -0400

    day 2 refactor

--- advent-of-code/day02.py
+++ advent-of-code/day02.py
@@ -1,52 +1,45 @@
-def checker(filename):
+def opener(filename):
     with open(filename) as file:
-        count = 0
-        for line in file:
-            line = line.strip().split()  
-            
-            # if the line is valid 
-            if doub_checker(line):
-                count += 1
-                continue #skip removing each value
-            
-            #removing one element at a time and check if it becomes valid
-            valid = False
-            for i in range(len(line)):
-                new_line = line[:i] + line[i+1:]  # remove the element at index i
-                if doub_checker(new_line):
-                    valid = True 
-                    break #get out of for loop so the new line doesn't become False
+        return [line.strip().split() for line in file]
 
-            if valid:
-                count += 1  # if we found a valid sequence after removing one element
-
-        
-        print(count)
-
-def doub_checker(line):
-    inc = False
-    dec = False
+def check_line(line):
     valid = True  # if the line is valid
+    diff = int(line[1]) - int(line[0])
+    # dec
+    if diff < 0:
+        for num in range(1, len(line)):
+            diff = int(line[num]) - int(line[num - 1])
+            if diff > -1 or diff < -3:
+                valid = False
 
-    # check the differences between consecutive elements
-    for num in range(1, len(line)):
-        diff = int(line[num]) - int(line[num-1])
-
-        # check if the difference is between 1 and 3 (inclusive)
-        if 1 <= abs(diff) <= 3:
-            if diff > 0:  # if the sequence is increasing
-                if dec:  # if it was previously decreasing, it's invalid
-                    valid = False
-                    break
-                inc = True
-            elif diff < 0:  # if the sequence is decreasing
-                if inc:  # if it was previously increasing, it's invalid
-                    valid = False
-                    break
-                dec = True
-        else:
-            valid = False
-            break  # no valid sequence if the difference is not between 1 and 3
-
+    # inc
+    elif diff > 0:
+        for num in range(1, len(line)):
+            diff = int(line[num]) - int(line[num - 1])
+            if diff < 1 or diff > 3:
+                valid = False
+    else:
+        valid = False
     return valid
-checker("inputfile.txt")
+
+
+def main():
+    lines = opener("inputfile.txt")
+    pt1 = sum(check_line(line) for line in lines)
+    print(pt1)
+    pt2 = 0
+    for line in lines:
+        if check_line(line):
+            pt2 += 1
+        else:
+            for num in range(len(line)):
+                # copy of line
+                moddedline = line[:]
+                moddedline.pop(num)
+                if check_line(moddedline):
+                    pt2 += 1
+                    break
+    print(pt2)
+
+
+main()
